8512659b576df550d4bd5590c8949b13ca160649
DemishD <rulimon23@yandex.ru>
2024-07-03 16:28:20 +0300

    Добавила фотки на комментах

--- src/Components/Dresses/Comment.js
+++ src/Components/Dresses/Comment.js
@@ -30,7 +30,7 @@ const Comment = ({
   return (
     <div key={comment.id} className="comment">
       <div className="comment-image-container">
-        <img src="/user-icon.png" />
+        <img src={comment.src1} />
       </div>
       <div className="comment-right-part">
         <div className="comment-content">
--- src/Components/api.js
+++ src/Components/api.js
@@ -2,6 +2,7 @@ export const getComments = async () => {
     return [
       {
         id: "1",
+        src1:"https://bronk.club/uploads/posts/2024-01/1705941716_bronk-club-p-smeshnie-momenti-s-kotami-pinterest-39.jpg",
         body: "Платье красивое. Село по фигуре. Не маломерит.",
         username: "Мария",
         userId: "1",
@@ -10,6 +11,7 @@ export const getComments = async () => {
       },
       {
         id: "2",
+        src1:"https://klopik.com/uploads/posts/2022-11/1617929573_34-p-schastlivii-kotik-36.jpg",
         body: "Платье легкое. Брала на море. Сидит идеально.",
         username: "Инна",
         userId: "2",
@@ -18,6 +20,7 @@ export const getComments = async () => {
       },
       {
         id: "3",
+        src1:"https://kartinkof.club/uploads/posts/2022-04/1649851327_42-kartinkof-club-p-rzhachnie-kartinki-s-kotyatami-45.jpg",
         body: "Спасибо за комментарий. Ваше мнение важно для нас.",
         username: "Магазин",
         userId: "2",
@@ -26,6 +29,7 @@ export const getComments = async () => {
       },
       {
         id: "4",
+        src1:"https://kartinkof.club/uploads/posts/2022-04/1649851327_42-kartinkof-club-p-rzhachnie-kartinki-s-kotyatami-45.jpg",
         body: "Спасибо за комментарий. Ваше мнение важно для нас.",
         username: "Магазин",
         userId: "2",
@@ -41,6 +45,7 @@ export const getComments = async () => {
       body: text,
       parentId,
       userId: "1",
+      src1:"https://koshka.top/uploads/posts/2021-12/1638584723_6-koshka-top-p-melkii-kotenok-7.jpg",
       username: "Карина",
       createdAt: new Date().toISOString(),
     };
